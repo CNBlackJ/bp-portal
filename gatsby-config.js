c7be6bf76758445839fcb0112931a32ffd9a08c3
vinli <vinli@digitalgd.com.cn>
2020-12-07 18:31:46 +0800

    feat: using new template

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,45 +1,10 @@
 module.exports = {
-  siteMetadata: {
-    title: `Digital BP Portal`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `vinli`,
-  },
   plugins: [
-    `gatsby-plugin-react-helmet`,
     {
-      resolve: `gatsby-source-filesystem`,
+      resolve: `gatsby-theme-codebushi`,
       options: {
-        name: `images`,
-        path: `${__dirname}/src/images`,
-      },
-    },
-    {
-      resolve: 'gatsby-source-strapi',
-      options: {
-        apiURL: 'http://10.33.122.138:1337',
-        contentTypes: [
-          'article',
-          'user'
-        ],
-        queryLimit: 1000,
-      },
-    },
-    `gatsby-transformer-sharp`,
-    `gatsby-plugin-sharp`,
-    {
-      resolve: `gatsby-plugin-manifest`,
-      options: {
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
-        start_url: `/`,
-        background_color: `#663399`,
-        theme_color: `#663399`,
-        display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
-      },
-    },
-    // this (optional) plugin enables Progressive Web App + Offline functionality
-    // To learn more, visit: https://gatsby.dev/offline
-    `gatsby-plugin-offline`,
-  ],
-}
+        tailwindConfig: `tailwind.config.js`
+      }
+    }
+  ]
+};
